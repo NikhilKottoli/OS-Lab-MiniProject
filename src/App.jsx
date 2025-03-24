e83d4be0a8f7ea7a620642739dcabd1fe64d3d11
Personal-123507 <padmavatikudal2025@gmail.com>
2025-03-24 20:59:09 +0530

    Added Memory Management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import About from "./pages/About";
+import MVT from "./pages/Memory_Management/Mvt";
+import MFT from "./pages/Memory_Management/Mft";
+import MemoryAllocationHome from "./pages/Memory_Management/MemoryHome";
+
 const App = () => {
   return (
-    <>
-      <div>
-        <About />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/mvt" element={<MVT />} />
+        <Route path="/mft" element={<MFT />} />
+        <Route path="/memoryhome" element={<MemoryAllocationHome />} />
+      </Routes>
+    </Router>
   );
 };
+
 export default App;
